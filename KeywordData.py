29858839b102beb85a2cacda55a958fd1c6ef33a
Tristan <kraig.tri125@gmail.com>
2017-09-27 20:37:02 -0400

    Attempting to follow PEP8 coding style.

--- KeywordData.py
+++ KeywordData.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import GwentUtils
 
-class KeywordData:
-    def __init__(self, gwentDataHelper):
-        self._helper = gwentDataHelper
 
-    def createKeywordJson(self):
+class KeywordData:
+    def __init__(self, gwent_data_helper):
+        self._helper = gwent_data_helper
+
+    def create_keyword_json(self):
         keywords = {}
         for locale in GwentUtils.LOCALES:
             keywords[locale] = self._helper.getKeywordTooltips(locale)
