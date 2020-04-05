44513e2c95b72f372952432d8efa5f7421403d10
Jamie Adkins <jamie.adkins@pulselive.com>
2020-04-05 23:48:33 +0100

    Fixes #38 for Saul de Navarette

--- GwentUtils.py
+++ GwentUtils.py
@@ -74,7 +74,7 @@ def _get_evaluated_tooltips(raw_tooltips, card_names, card_abilities, card_templ
             tooltips[card_id] = tooltips[card_id].replace(key, value)
 
         # https://github.com/GwentCommunityDevelopers/gwent-data/issues/38
-        tooltips[card_id] = tooltips[card_id].replace("Hoard-B.P.BB_Hoard", "Hoard")
+        tooltips[card_id] = tooltips[card_id].replace("-B.P.BB_Hoard", "")
         tooltips[card_id] = tooltips[card_id].replace("Tribute-B.P.BB_Tribute", "Tribute")
 
         # Now replace all the other card abilities.
